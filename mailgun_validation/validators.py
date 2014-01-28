6f0b227f3292d616cbb71b5ae4cb1f51cc954403
Antonio Ognio <antonio@ognio.com>
2014-01-27 23:41:17 -0500

    Invoking the Mailgun webservice using requests with no extra dependency

--- mailgun_validation/validators.py
+++ mailgun_validation/validators.py
@@ -1,35 +1,45 @@
+from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _, ungettext_lazy
 from django.utils.encoding import force_text
-from django.conf import settings
-
-from mailgun.api import MailgunAPI
-from mailgun.api import MailgunException
+from mailgun_validation.exceptions import MailgunException
+import requests
 
 class EmailValidator(object):
     message = _('Enter a valid email address.')
     code = 'invalid'
 
-    def __init__(self, message=None, code=None, whitelist=None, api_key=None):
+    def __init__(self, message=None, code=None, api_key=None):
+        self.api_key = api_key
         if message is not None:
             self.message = message
         if code is not None:
             self.code = code
-        if whitelist is not None:
-            self.domain_whitelist = whitelist
 
     def __call__(self, value):
         value = force_text(value)
 
-        if api_key is None:
+        if self.api_key is None:
             raise MailgunException('No API key was provided.')
 
-        mailgun = MailgunAPI(api_key=api_key)
-        result = mailgun.validate_address(value)
+        validated = self.validate_address(value)
 
-        if result['is_valid'] == False:
+        if not validated:
             raise ValidationError(self.message, code=self.code)
 
+    def validate_address(self, email):
+        response = requests.get(
+            "https://api.mailgun.net/v2/address/validate",
+            auth=("api", self.api_key),
+            params={"address": email}
+        )
+        if response.ok:
+            response_data = response.json()
+            return response_data['is_valid']
+        else:
+            raise MailgunException('Problems with the Mailgun web service')
+            
+        
 
 api_key = None
 if hasattr(settings, 'MAILGUN_API_KEY'):
