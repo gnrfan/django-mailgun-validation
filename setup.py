00aa75c19a3f0d1f5e0ffb1a63e7a1c8adc65fc8
Antonio Ognio <gnrfan@eternity-2.local>
2015-02-18 17:30:45 -0500

    Bumping version number to 1.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='django-mailgun-validation',
-    version='1.0',
+    version='1.0.1',
     description='Django fields for validating email using the Mailgun API',
     author='Antonio Ognio',
     author_email='antonio@ognio.com',
